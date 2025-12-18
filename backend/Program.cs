d1b00eba90849e971d0f95f829cd83c07aa74a15
Janidu Pabasara <141099578+JaniduP2003@users.noreply.github.com>
2025-12-18 08:50:54 +0530

    ADD::: new logic make a in memory db and add JWT
    how to ::
    first use the builder. its like a toolbox has what you need
    then make container thats the .services thats a container for the services no need to add the new key word
    
    the register it as .name<name>
    thern add a labada fuction ( name of the variable that holds the data => )
    pure labda function ( => )
    then use in mmeory database then add a neme to it
    
    What it is: The object used to configure your ASP.NET Core application before it starts.
    
    Why it exists: ASP.NET Core separates configuration from runtime. builder is the “setup toolkit.”

--- backend/Program.cs
+++ backend/Program.cs
@@ -8,6 +8,53 @@ using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
+//add the services here
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+
+//add swagger //this dose not add neyething just adds swagger to the apps
+builder.Services.AddSwaggerGen(c=>{
+    c.SwaggerDoc("v1",new OpenApiInfo{Title ="Secure Document API",Version ="v1"});
+    c.AddSecurityDefinition("Bearer",new OpenApiSecuirityScheme
+    {
+        Description ="Jwt Autorization Header USing Barrer scheme",
+        nameof="Authorization",
+        In=ParameterLocation.Header,
+        Type=SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+
+    });
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement{
+        {
+            new OpenApiSecurityScheme{
+                Referenc = new OpenApiRefrence{
+                    Type = referenceType.SequrityScheme,
+                    Id ="bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+
+
+});
+
+//needs to register the database so others can use it inject it into them 
+// first needs to register it as"App db context"
+//configer Ef core to run in memeory ne need to stor in a database 
+//🧠 What is AppDbContext? Defines database tables as C# classes. 
+// when the app is restared this data is removed
+
+builder.Services.AppDbContext<AppDbContext>( options =>
+       options.UseInMemoryDatabase("secureDocumentdb"));
+       //lamdafuctions has a vaeriable named options and after => its called 
+       // and added to use inmemerydatabse
+
+//read the key for the appsetting 
+var jwtKey = builder.Configuration["jwt:jwtKey"];
+
+
+
 
 
 
