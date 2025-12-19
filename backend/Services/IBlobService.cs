d22977a2ac6d67ee3e0494d576f1a6f13c62cd61
Janidu Pabasara <141099578+JaniduP2003@users.noreply.github.com>
2025-12-19 08:55:25 +0530

    ADD:::  the i blob service
    1️⃣ Task<T>
    
    Task means this method is asynchronous — it may take time (like uploading or downloading a file).its promise not instant it will take time
    
    IEnumerable<T> = a collection of items you can loop over.
    BlobFileInfo = the type of item in the collection (in this case, metadata about a file: name, size, content type, etc.)

---
+++ backend/Services/IBlobService.cs
@@ -0,0 +1,32 @@
+//Think of IBlobService as the job description of a 
+//post office clerk, and the files are packages.
+
+//Returns (BlobName, Url) → the tracking number and link to access the package. 
+
+// first need to get a blobname and url 
+//then using the blpbname mut donload a STREAM (a file)
+// and whe asked id then must show whats are inside the blob conatiner 
+// adn a delate using the blob name 
+//get emep links for 15mins
+// and make a bool to move to archive for longthwem storage
+
+namespace backend.Services;
+
+public interface IblobService{
+    Task<(string blobName , string Url)> uplodeAsync(string userId , IFormFile file);
+    Task<stream> DownloadAsync(string blobName);
+    Task<IEnumerable<BlobFileInfo>> ListFilesAsync(string userid); 
+    Task<bool> DeleteAsync(string blobName);
+    Task<string> GetSasUrlAsync(string blobName, int ExpiryMinutes =15);
+    Task<bool> ArchiveAsync(string blobName);
+}
+
+// above i made a blobfileinfr now lets make the attributes
+
+public class BlobFileInfo { 
+    public static Name {get;set;} = string.Empty;
+    public long Size {get;set;} 
+    public DateTimeOffset? LastModified {get;set;}
+
+    public string contentType {get;set; } =string.Empty;
+}
